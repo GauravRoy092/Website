30b73710b4842f5389c76e80dbe733f2533e4d11
Gaurav <royg48848@gmail.com>
2023-04-24 14:49:05 +0530

    customizer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
                 type="filled"
                 title="Customize It"
                 handleClick={() => state.intro = false}
-                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm btn-outline-danger"
               />
             </motion.div>
           </motion.div>
